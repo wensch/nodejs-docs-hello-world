369f74d97b8584d124008450a9ff0f55d2f49e6a
helcorin <helcorintho@gmail.com>
2017-09-10 01:32:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Testing again, I have a CI up and running, I hope so...");
+    response.end("Testing again, I have a CI up and running, I hope so.");
 
 });
 
